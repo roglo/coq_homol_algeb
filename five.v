bed21b3dcc50cf766fc19b18455c8164b07e64f4
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-05-28 18:56:29 +0200

    -

--- five.v
+++ five.v
@@ -7,16 +7,18 @@ Require Import AbGroup.
 
 Definition is_mono {A B} (f : HomGr A B) :=
   ∀ C (g₁ g₂ : HomGr C A),
-  (∀ x, Happ f (Happ g₁ x) = Happ f (Happ g₂ x))
-  → (∀ x, Happ g₁ x = Happ g₂ x).
+  (∀ x, (Happ f (Happ g₁ x) = Happ f (Happ g₂ x))%G)
+  → (∀ x, (Happ g₁ x = Happ g₂ x)%G).
 
 Definition is_epi {A B} (f : HomGr A B) :=
   ∀ C (g₁ g₂ : HomGr B C),
-  (∀ x, Happ g₁ (Happ f x) = Happ g₂ (Happ f x))
-  → (∀ y, Happ g₁ y = Happ g₂ y).
+  (∀ x, (Happ g₁ (Happ f x) = Happ g₂ (Happ f x))%G)
+  → (∀ y, (Happ g₁ y = Happ g₂ y)%G).
 
 Definition is_iso {A B} (f : HomGr A B) :=
-  ∃ g : HomGr B A, (∀ x, Happ g (Happ f x) = x) ∧ (∀ y, Happ f (Happ g y) = y).
+  ∃ g : HomGr B A,
+  (∀ x, (Happ g (Happ f x) = x)%G) ∧
+  (∀ y, (Happ f (Happ g y) = y)%G).
 
 (* Composition of homomorphims *)
 
@@ -96,8 +98,8 @@ move b₁ before s'; move d₁ before b₁.
 unfold is_iso.
 enough
   (H : ∃ c',
-   (∀ x : gr_set C, Happ c' (Happ c x) = x) ∧
-   (∀ y : gr_set C', Happ c (Happ c' y) = y)). {
+   (∀ x : gr_set C, (Happ c' (Happ c x) = x)%G) ∧
+   (∀ y : gr_set C', (Happ c (Happ c' y) = y)%G)). {
   destruct H as (c' & Hc'c & Hcc').
   now exists c'.
 }
@@ -105,9 +107,15 @@ assert
   (H : ∀ x', ∃ x, x' ∈ C' → x ∈ C ∧ (Happ h x = Happ d₁ (Happ h' x'))%G). {
   intros x'.
   destruct (MemDec C' x') as [Hx'| Hx'].
-  -assert (H : Happ d₁ (Happ h' x') ∈ Ker j). {
+  -assert (H : Happ j (Happ d₁ (Happ h' x')) ∈ Ker e). {
+     split; [ now apply j, d₁, h' | ].
+     rewrite Hcjj'.
+     etransitivity.
+     -apply j'; [ now apply d, d₁, h' | | apply Hdd₁ ].
+      now apply h'.
+     -idtac.
+(* perhaps should make a lemma for this case... *)
+...
+   assert (H : Happ d₁ (Happ h' x') ∈ Ker j). {
      split; [ now apply d₁, h' | ].
-     specialize (Hme _ (Hcomp j (Hcomp d₁ h'))) as H1.
-(* faire plutôt le morphisme de 0 dans E
-   ouais, enfin bon, faut voir... *)
 ...
