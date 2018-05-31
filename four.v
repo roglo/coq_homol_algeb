3471c8c9c8f17520d81bcedcbe1be8d6f6335e7b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-05-31 02:09:01 +0200

    -

--- four.v
+++ four.v
@@ -8,9 +8,9 @@ Require Import AbGroup Setoid.
 (* Four lemma #1
            g       h        j
         B------>C------>D------>E
-        |       |       |       |
+        |       |       |       Γ
        b|      c|      d|      e|
-        |       |       |       |
+        v       |       v       |
         v       v       v       v
         B'----->C'----->D'----->E'
            g'      h'      j'
@@ -48,4 +48,33 @@ enough
 intros * Hgc *.
 unfold is_epi in Heb, Hed.
 unfold is_mono in Hme.
+assert (H : ∃ t, (Happ d t = Happ h' z')%G). {
+  assert (Hn : ¬ (∀ t, (Happ d t ≠ Happ h' z')%G)). {
+    Axiom EqDec : ∀ {A}, ∀x y : gr_set A, {(x = y)%G} + {(x ≠ y)%G}.
+    set (v d' := if EqDec d' (Happ h' z') then true else false).
+    set (w (d' : gr_set D') := false).
+    specialize (Hed _ v w) as H1.
+    intros H2.
+    assert (H : ∀ x : gr_set D, v (Happ d x) = w (Happ d x)). {
+      intros t.
+      unfold v, w.
+      destruct (EqDec (Happ d t) (Happ h' z')) as [H| H]; [ | easy ].
+      now specialize (H2 t).
+    }
+    specialize (H1 H (Happ h' z')).
+    unfold v, w in H1.
+    destruct (EqDec (Happ h' z') (Happ h' z')) as [H3| H3]; [ easy | ].
+    now apply H3.
+  }
+  Axiom NNE : ∀ A (P : A → Prop), (¬ ¬ ∃ x, P x) → ∃ x, P x.
+  apply NNE.
+  intros H2.
+  apply Hn; intros t H3.
+  apply H2.
+  now exists t.
+}
+(* ok but, as I already knew, I had to add two axioms.
+   solutions:
+   -either accept them in the global proof,
+   -or define is_epi as "is a surjection" (∀ ∃ ...) *)
 ...
